6ad96edb992360e4b5d9175ad1c03c28b6c2114f
Deonis Butuc <deonis.butuc@endava.com>
2023-09-11 16:25:05 +0300

    schimbari importante

--- Draft2Class.java
+++ Draft2Class.java
@@ -1,5 +1,7 @@
 public class Draft2Class {
     public static void main(String[] args) {
         System.out.println("sadasda");
+        System.out.println("dashdkasjd");
+        System.out.println("asdj;asjda");
     }
 }
