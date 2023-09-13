4f3a319a4024f7de9cec0a7435c5cd17505b9f40
DButuc <dbutuc91@gmail.com>
2023-09-13 20:10:37 +0300

    Git work flow

--- Draft2Class.java
+++
@@ -1,7 +0,0 @@
-public class Draft2Class {
-    public static void main(String[] args) {
-        System.out.println("sadasda");
-        System.out.println("dashdkasjd");
-        System.out.println("asdj;asjda");
-    }
-}
