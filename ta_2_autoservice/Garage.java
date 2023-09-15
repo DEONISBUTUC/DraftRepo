a4a43a980a119f294cde8d274342a3cb468499bf
DButuc <dbutuc91@gmail.com>
2023-09-15 22:09:44 +0300

    Add the Auto Service functionality

---
+++ ta_2_autoservice/Garage.java
@@ -0,0 +1,12 @@
+package ta_2_autoservice;
+
+public class Garage {
+    public String name;
+    public String address;
+    public int postalCode;
+    public int phoneNumber;
+    public Garage(){
+        System.out.println("Toate informatiile utile vor fi afisate in scurt timp");
+    }
+    }
+
---
+++ ta_2_autoservice/Handler.java
@@ -0,0 +1,23 @@
+package ta_2_autoservice;
+
+public class Handler {
+    public static void main(String[] args) {
+        Car car1 = new Car("Audi","CRT 125",25545);
+        Car car2 = new Car("Bmw","CAT 745",49646);
+        Car car3 = new Car("Renault","CQW 952",97946);
+
+        Garage autoDoctor = new Garage();
+        autoDoctor.name = "Autodoctor";
+        autoDoctor.address = "Stefan cel Mare 32";
+        autoDoctor.phoneNumber = 79025874;
+        autoDoctor.postalCode = 24850;
+
+        Worker worker1 = new Worker();
+        worker1.name = "Ion";
+        worker1.isAvailable = false;
+
+        System.out.println("Bine ati venit la " + autoDoctor.name + " care este situat pe strada: " + autoDoctor.address);
+        System.out.println("In scurt timp masina dumnevoastra de marca " + car1.name + " cu numarul de imatriculare " + car1.plateNumber + " va fi luata in lucrul de mecanicul " + worker1.name );
+        System.out.println("In caz ca aveti careva intrebari ne puteti contacta la nr de tel " + autoDoctor.phoneNumber);
+    }
+}
